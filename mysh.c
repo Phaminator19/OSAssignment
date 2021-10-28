11fa253a43dfcd92bc860c54cbebf662f5fd027d
Phaminator <qpham8@knights.ucf.edu>
2021-10-28 17:14:20 -0400

    commit with the group member

--- mysh.c
+++ mysh.c
@@ -1,4 +1,4 @@
-/*COP4600 - Homework_2 
+/*COP4600 - Homework_2
 /*myshell - making my own linux shell in C */
 #include <stdio.h>
 #include <string.h>
@@ -11,6 +11,7 @@
 #include <dirent.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <stdbool.h>
 
 #define MAXLIST 10000 //max number of letters to be supported
 #define MAXCOM 100 // max number of commands to be supported
@@ -18,8 +19,9 @@
        
 char* stack[MAXCOM];     
 int top = -1;
-FILE *history;            
+FILE *historyFile;            
 
+// Made by Zac
 int isempty() {
 
    if(top == -1)
@@ -27,7 +29,8 @@ int isempty() {
    else
       return 0;
 }
-   
+
+// Made by Zac   
 int isfull() {
 
    if(top == MAXCOM)
@@ -36,12 +39,19 @@ int isfull() {
       return 0;
 }
 
-int replay(int num) {
+// Made by Zac
+char* replay(int num) {
+    if(num > top || num < 0)
+    {
+        printf("Command does not exist. Please use a valid command.");
+        return NULL;
+    }
    return stack[top - num];
 }
 
+// Made by Zac
 char* popCommand() {
-   int data;
+   char* data;
 	
    if(!isempty()) {
       data = stack[top];
@@ -52,7 +62,8 @@ char* popCommand() {
    }
 }
 
-int pushCommand(char* data) {
+// Made by Zac
+char* pushCommand(char* data) {
 
    if(!isfull()) {
       top = top + 1;   
@@ -92,16 +103,16 @@ char *read_line(void) {
     size_t len = 0;
     ssize_t read;    
 
-    if(history = fopen("history.txt", "rw"))
+    if(historyFile = fopen("history.txt", "r+"))
     {
-        while ((read = getline(&line, &len, history)) != -1) 
+        while ((read = getline(&line, &len, historyFile)) != -1) 
         {
-            pushCommand(read);
+            pushCommand(line);
         }
     }
     else
     {
-        history = fopen("history.txt", "a");
+        historyFile = fopen("history.txt", "w+");
     }
     
     while(1) {
@@ -197,6 +208,7 @@ void process_commands(void) {
     while(1) {
         printf("# ");
         buffer = read_line();
+        pushCommand(buffer);
         
         command = parse_command_line(buffer);
         execArgs(command);
@@ -226,59 +238,28 @@ void movetoDir(char *Directory) {
 void whereami() {
     printf("%s\n", currentdir);
 }
-// Made by Quang Pham
-// struct stack_struct {
-//     int top;
-//     unsigned capacity;
-//     char* commands;
-// };
 
-// typedef struct stack_struct stack;
+// Made by Zac
+// Clears history when -c flag is given
+void clearHistory()
+{
+    while(!isempty())
+    {
+        popCommand();
+    }
+    historyFile = fopen("history.txt", "w");
+}
 
-// stack *createStack(unsigned capacity) {
-//     stack *st = malloc(sizeof(stack));
-
-//     st->capacity = capacity;
-//     st->top = -1;
-//     st->commands = (char*)malloc(st->capacity * sizeof(char));
-
-//     return st;
-// }
-
-// int is_empty(stack *st) {
-//     return st->top == -1; 
-// }
-
-// int is_full(stack *st) {
-//     return st->top == st->capacity - 1;
-// }
-
-// stack *pop (stack *st) {
-//     if(is_empty(st)) {
-//         return INT_MIN;
-//     }
-
-//     return st->commands[st->top--];
-
-    
-// }
-
-// void push(stack *st, char* commands) {
-    
-//     if(is_full(st)) {
-//         printf("the stack is full\n");
-//         return;
-//     }
-//     else {
-//         increment = st->top + 1;
-//         st->commands[increment] = commands;
-//     }
-// }
-
-
-
+// Made by Zac
 //prints out the recently typed commands in reverse order with numbers.
-void history() {
+void checkHistory(bool flag) {
+    
+    if (flag)
+    {
+        clearHistory();
+        return;
+    }
+    
     int i = 0;
     while(i <= top)
     {
@@ -287,24 +268,15 @@ void history() {
     }
 }
 
-// Clears history when -c flag is given
-void clearHistory()
-{
-    while(!isempty())
-    {
-        popCommand();
-    }
-    history = fopen("history.txt", "w");
-}
-
+// Made by Zac
 // Terminates shell and saves history file
 void byebye()
 {
     while(!isempty())
     {
-        fprintf(history, "%s\n", popCommand())
+        fprintf(historyFile, "%s\n", popCommand());
     }
-    fclose(history);
+    fclose(historyFile);
     exit(0);
 }
 
@@ -314,20 +286,110 @@ void dalek(int pid)
 }
 
 //made by Quang
+// is this relative or direct path?
 void start(char *program_name) {
     int pid = fork();
-    // 
+    char *argv[] = {program_name, NULL};
+    
+    // Find result of fork call.
+    if(pid < 0)
+    {
+        perror("FORK FAILED\n");
+        return;
+    }
+    
+    else if (pid == 0) {
     //this would check if the argument starts with "/" the shell should interpret it as full path then.
     if (strchr(program_name, '/') != NULL) {
-        //execv
-        if (pid == 0) {
-            char *argv[3] = {, ,NULL};
-            execv(program_name[0], )
+            // Edits made by Joey
+            printf("Starting program: %s\n", program_name);
+            execv(argv[0], argv);
         }
     }
-    else {
-        //execve
+    //if the argument isn't start with "/ the shell should interpreted as a relative path."
+    else if (strchr(program_name, '/') == NULL) {
+        printf("Starting program: %s\n", program_name);
+        execv(argv[0], argv);
     }
+    else {
+        perror("the execution failed.\n");
+    }
+
+    //check to see if we are in the parent process
+    if (pid>0) {
+        waitpid(pid, NULL, 0);
+    }
+    if (pid == 0) {
+        perror("the given program could not be found\n");
+
+        return;
+    }
+}
+
+//It is similar to the run command, but it immediately prints the PID of the program it 
+// started, and returns the prompt. 
+// Execute a program in the background
+
+
+
+int ownCmdHandler(char **parsed) {
+    int NoOfOwnCmds = 5, i, switchOwnArg = 0;
+    char* ListOfOwnCmds[NoOfOwnCmds];
+    char* username;  
+  
+    ListOfOwnCmds[0] = "byebye";
+    ListOfOwnCmds[1] = "movetodir";
+    ListOfOwnCmds[2] = "help";
+    ListOfOwnCmds[3] = "hello";
+    ListOfOwnCmds[4] = "history";
+    ListOfOwnCmds[5] = "replay";
+    ListOfOwnCmds[6] = "";
+  
+    for (i = 0; i < NoOfOwnCmds; i++) {
+        if (strcmp(parsed[0], ListOfOwnCmds[i]) == 0) {
+            switchOwnArg = i + 1;
+            break;
+        }
+    }
+  
+    switch (switchOwnArg) {
+    case 1:
+        printf("\nGoodbye\n");
+        exit(0);
+    case 2:
+        if (sizeof(parsed) / sizeof(parsed[0]) > 1)
+        {
+            chdir(parsed[1]);
+        }
+        else
+        {
+            printf("Incorrect number of arguments.");
+        }
+        return 1;
+    case 3:
+        // openHelp();
+        return 1;
+    case 4:
+        // username = getenv("USER");
+        // printf("\nHello %s.\nMind that this is "
+        //     "not a place to play around."
+        //     "\nUse help to know more..\n",
+        //     username);
+        return 1;
+    case 5:
+        if(sizeof(parsed) / sizeof(parsed[0]) > 1 && strcmp(parsed[1], "-c") == 0)
+        {
+            checkHistory(true);
+        }
+        else
+        {
+            checkHistory(false);
+        }
+    default:
+        break;
+    }
+  
+    return 0;
 }
 
 int main() {
